50264ab27f4d03c5f8bddd678c261cc0a5f9d3b9
hqfa-666 <1523424779@qq.com>
2026-09-12 12:41:38 +0800

    Add files via upload

---
+++ c1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+
+{
+	//printf("helloworld\n");
+	//printf("%c\n", 'x');
+	//printf("%c\n", '\130');
+	//printf("%c\n", 81);
+	//printf("%c\n", '\121');
+	//printf("%c\n", '\x51');
+	//printf("%s\n", "asb");
+	//_Bool flag=true;
+	//if (flag)
+	//	printf("l like c");
+	//("%zu", sizeof(_Bool));
+	printf("%zu", sizeof(double));
+
+	return;
+}
